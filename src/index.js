f0c1a725f05175a951866004f5cb1d038e9591df
yura1014 <62890040+yura1014@users.noreply.github.com>
2021-07-25 20:07:09 +0300

    feat: implement toReadable body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function toReadable (number) {
-  
+   numbArray = String(numb).split("");
+  console.log(numbArray);
+  let output = "";
+  numbArray.forEach((element) => {
+    switch (element) {
+      case "0":
+        output = output + "null ";
+        break;
+      case "1":
+        output = output + "one ";
+        break;
+      case "2":
+        output = output + "two ";
+        break;
+      case "3":
+        output = output + "three ";
+        break;
+      case "4":
+        output = output + "four ";
+        break;
+      case "5":
+        output = output + "five ";
+        break;
+      case "6":
+        output = output + "six ";
+        break;
+      case "7":
+        output = output + "seven ";
+        break;
+      case "8":
+        output = output + "eight ";
+        break;
+      case "9":
+        output = output + "nine ";
+        break;
+    }
+  });
+  return output;
 }
